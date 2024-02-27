725c189598f74fa43c2bdc69fa179a98d84c6447
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-27 22:46:14 +0700

    [FEATURE] Add Customer Entity

---
+++ src/main/java/com/enigma/wmb_api/entity/Customer.java
@@ -0,0 +1,28 @@
+package com.enigma.wmb_api.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="m_customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name="customer_name", columnDefinition = "VARCHAR(50)")
+    private String customerName;
+
+    @Column(name="mobile_phone_no", columnDefinition = "VARCHAR(20)")
+    private String mobilePhoneNo;
+
+    @Column(name="is_member")
+    private Boolean isMember;
+
+}
