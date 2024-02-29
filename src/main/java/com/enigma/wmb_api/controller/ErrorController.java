bb036bc649d19ec3f71b7cde1e9d5219b4a32581
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-29 10:18:15 +0700

    [FEATURE] Add Errorhandler

--- src/main/java/com/enigma/wmb_api/controller/ErrorController.java
+++ src/main/java/com/enigma/wmb_api/controller/ErrorController.java
@@ -1,5 +1,6 @@
 package com.enigma.wmb_api.controller;
 
+import com.enigma.wmb_api.dto.response.CommonResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,7 +10,12 @@ import org.springframework.web.server.ResponseStatusException;
 public class ErrorController {
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<?> responseExceptionHandler(ResponseStatusException exception){
-
+        CommonResponse<?> commonResponse=CommonResponse.builder()
+                .statusCode(exception.getStatusCode().value())
+                .message(exception.getReason())
+                .build();
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(commonResponse);
     }
 
 }
