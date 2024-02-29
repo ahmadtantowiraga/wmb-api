5998874c96a6af6482c10bdf18ce525739340072
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-29 09:26:03 +0700

    [FEATURE] Add SearchCustomerRequest

--- src/main/java/com/enigma/wmb_api/dto/request/SearchCustomerRequest.java
+++ src/main/java/com/enigma/wmb_api/dto/request/SearchCustomerRequest.java
@@ -1,4 +1,15 @@
 package com.enigma.wmb_api.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SearchCustomerRequest {
+    private String customerName;
+    private String mobilePhoneNo;
+    private Integer page;
+    private Integer size;
 }
