bd94d1e3db83442e58358682dac9f465413e0cc6
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-29 10:12:24 +0700

    [FEATURE] Add PagingResponse

---
+++ src/main/java/com/enigma/wmb_api/controller/ErrorController.java
@@ -0,0 +1,15 @@
+package com.enigma.wmb_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> responseExceptionHandler(ResponseStatusException exception){
+
+    }
+
+}
