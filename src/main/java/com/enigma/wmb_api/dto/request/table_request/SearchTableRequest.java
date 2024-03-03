9492453be9b4d02a3db7add225423b11e68190bb
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-03 09:55:54 +0700

    [FEATURE] Add SearchTableRequest

---
+++ src/main/java/com/enigma/wmb_api/dto/request/table_request/SearchTableRequest.java
@@ -0,0 +1,9 @@
+package com.enigma.wmb_api.dto.request.table_request;
+
+public class SearchTableRequest {
+    private Integer page;
+    private Integer size;
+    private String direction;
+    private String sortBy;
+    private String tableName;
+}
--- src/main/java/com/enigma/wmb_api/service/TableService.java
+++ src/main/java/com/enigma/wmb_api/service/TableService.java
@@ -2,6 +2,7 @@ package com.enigma.wmb_api.service;
 
 
 import com.enigma.wmb_api.dto.request.table_request.NewTableRequest;
+import com.enigma.wmb_api.dto.request.table_request.SearchTableRequest;
 import com.enigma.wmb_api.dto.request.table_request.UpdateTableRequest;
 import com.enigma.wmb_api.entity.Tables;
 import org.springframework.data.domain.Page;
