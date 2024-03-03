e3d2eb1b109f30a1863b865fef8cffea0c5fd57c
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-03 13:31:39 +0700

    [FEATURE] Add TransactionTypeServiceImpl

--- src/main/java/com/enigma/wmb_api/service/impl/TransactionTypeServiceImpl.java
+++ src/main/java/com/enigma/wmb_api/service/impl/TransactionTypeServiceImpl.java
@@ -1,9 +1,37 @@
 package com.enigma.wmb_api.service.impl;
 
+import com.enigma.wmb_api.dto.request.transaction_type_request.NewTransactionTypeRequest;
+import com.enigma.wmb_api.dto.request.transaction_type_request.SearchTransactionTypeRequest;
+import com.enigma.wmb_api.entity.TransactionType;
+import com.enigma.wmb_api.repository.TransactionTypeRepository;
+import com.enigma.wmb_api.service.TransactionTypeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
-public class TransactionTypeServiceImpl {
+public class TransactionTypeServiceImpl implements TransactionTypeService {
+    private final TransactionTypeRepository transactionTypeRepository;
+    @Override
+    public TransactionType findById(String id) {
+        return transactionTypeRepository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST, "TransactionType is not found"));
+    }
+
+    @Override
+    public TransactionType update(NewTransactionTypeRequest request) {
+        return null;
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public Page<TransactionType> findAll(SearchTransactionTypeRequest request) {
+        return null;
+    }
 }
