dec6dcc629850d032e2703186577cac9c30a3c8e
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-03 13:26:58 +0700

    [FEATURE] Add TransactionTypeService

---
+++ src/main/java/com/enigma/wmb_api/dto/request/transaction_type_request/SearchTransactionTypeRequest.java
@@ -0,0 +1,16 @@
+package com.enigma.wmb_api.dto.request.transaction_type_request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SearchTransactionTypeRequest {
+    private Integer page;
+    private Integer size;
+    private String direction;
+    private String sortBy;
+    private String tableName;
+}
--- src/main/java/com/enigma/wmb_api/service/TransactionTypeService.java
+++ src/main/java/com/enigma/wmb_api/service/TransactionTypeService.java
@@ -1,10 +1,15 @@
 package com.enigma.wmb_api.service;
 
 import com.enigma.wmb_api.constant.TransactionTypeID;
+import com.enigma.wmb_api.dto.request.transaction_type_request.NewTransactionTypeRequest;
+import com.enigma.wmb_api.dto.request.transaction_type_request.SearchTransactionTypeRequest;
 import com.enigma.wmb_api.entity.TransactionType;
+import org.springframework.data.domain.Page;
 
 public interface TransactionTypeService {
     TransactionType findById(String id);
-    TransactionType update();
+    TransactionType update(NewTransactionTypeRequest request);
+    void delete(String id);
+    Page<TransactionType> findAll(SearchTransactionTypeRequest request);
 
 }
