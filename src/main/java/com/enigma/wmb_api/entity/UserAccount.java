fe6b05508b045ddf9b775f61ddab8a4b77737096
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-09 10:14:00 +0700

    [FEATURE] Add UserAccount Entity

--- src/main/java/com/enigma/wmb_api/entity/UserAccount.java
+++ src/main/java/com/enigma/wmb_api/entity/UserAccount.java
@@ -1,9 +1,15 @@
 package com.enigma.wmb_api.entity;
 
 import com.enigma.wmb_api.constant.ConstantTable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.enigma.wmb_api.constant.UserRole;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,11 +18,44 @@ import lombok.*;
 @Builder
 @Entity
 @Table(name = ConstantTable.USER_ACCOUNT_TABLE)
-public class UserAccount {
+public class UserAccount implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    private String username;
-    private String password;
-    private Boolean isEnable;
-    private
 
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name= "is_enable")
+    private Boolean isEnable;
+
+    @ManyToMany(fetch =FetchType.EAGER)
+    private List<Role> role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.stream().map(role1 -> new SimpleGrantedAuthority(role1.getRole().name())).toList();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnable;
+    }
 }
