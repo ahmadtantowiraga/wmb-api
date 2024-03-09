294e9e0fd84638aaa3d937b6866680e3abe048f6
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-09 10:49:11 +0700

    [FEATURE] Add AuthServiceImpl

---
+++ src/main/java/com/enigma/wmb_api/service/impl/AuthServiceImpl.java
@@ -0,0 +1,23 @@
+package com.enigma.wmb_api.service.impl;
+
+import com.enigma.wmb_api.dto.request.AuthRequest;
+import com.enigma.wmb_api.dto.response.LoginResponse;
+import com.enigma.wmb_api.dto.response.RegisterResponse;
+import com.enigma.wmb_api.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+    @Override
+    public RegisterResponse register(AuthRequest request) {
+        return null;
+    }
+
+    @Override
+    public RegisterResponse registerAdmin(AuthRequest request) {
+        return null;
+    }
+
+    @Override
+    public LoginResponse login(AuthRequest request) {
+        return null;
+    }
+}
