cc471bb61c9c37a8aa89a80c926c30a4b5591456
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-09 10:31:27 +0700

    [FEATURE] Add RoleServiceImpl

---
+++ src/main/java/com/enigma/wmb_api/service/impl/RoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.enigma.wmb_api.service.impl;
+
+import com.enigma.wmb_api.constant.UserRole;
+import com.enigma.wmb_api.entity.Role;
+import com.enigma.wmb_api.repository.RoleRepository;
+import com.enigma.wmb_api.service.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Role getOrSave(UserRole role) {
+        return roleRepository.findByRole(role).orElseGet(()->roleRepository.saveAndFlush(Role.builder().role(role).build()));
+    }
+}
