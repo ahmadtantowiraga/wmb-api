643a4e53c938c91764e6c8a746b318af9c027b4a
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-03-09 14:39:20 +0700

    [FEATURE] Add TableREsponse

---
+++ src/main/java/com/enigma/wmb_api/dto/response/MenuResponse.java
@@ -0,0 +1,18 @@
+package com.enigma.wmb_api.dto.response;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MenuResponse {
+    private String id;
+    private String menuName;
+    private Long price;
+}
